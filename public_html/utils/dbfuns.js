975bd9494bc2023d73b62599432c6790c92a455a
Varun Gupta <varun.gupta1798@gmail.com>
2017-03-16 23:23:12 +0530

    dbfile generated

---
+++ public_html/utils/dbfuns.js
@@ -0,0 +1,32 @@
+/**
+ * Created by varun on 3/16/17.
+ */
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize("cb","cbUser","cbPass",{
+    host : 'localhost',
+    dialect : 'mysql',
+});
+
+// TODO table to store courses
+
+
+
+
+
+
+
+// TODO table to store assignments
+
+
+
+
+
+
+//TODO archive table for courses
+
+
+
+module.exports = {
+
+}
