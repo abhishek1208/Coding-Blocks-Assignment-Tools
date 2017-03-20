9a0add43b25dbce79d43e288a2cd5751aa56f3dd
Abhishek Yadav <abhishekgit1208@gmail.com>
2017-03-20 23:57:07 +0530

    Front end for addAssignment added!

---
+++ public_html/AddAssignment/index.js
@@ -0,0 +1,57 @@
+/**
+ * Created by abhishekyadav on 20/03/17.
+ */
+
+
+var assgn_name=$('#assgn_name')
+var assgn_desc=$('#assgn_desc')
+var assgn_courseid=$('#assgn_courseid')
+var submit = $('#submit');
+
+
+$(document).ready(function() {
+    $('#assignmentForm').bootstrapValidator({
+        container: '#messages',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            name: {
+                validators: {
+                    notEmpty: {
+                        message: 'The name is required and cannot be empty'
+                    }
+                }
+            },
+            desc: {
+                validators: {
+                    notEmpty: {
+                        message: 'The Description is required and cannot be empty'
+                    }
+                }
+            }
+            ,
+            courseid: {
+                validators: {
+                    notEmpty: {
+                        message: 'The courseid is required and cannot be empty'
+                    }
+                }
+            }
+        }
+    });
+});
+
+submit.click(function (ev) {
+
+
+    $.post('http://localhost:4000/addAssignment/add', {
+        name:assgn_name.val(),
+        desc:assgn_desc.val(),
+        courseid: assgn_courseid.val()
+    })
+
+
+})
--- routers/addAssignmentRouter.js
+++ routers/addAssignmentRouter.js
@@ -3,42 +3,55 @@
  */
 
 //abhishek's code
+const express = require('express');
+const bp = require('body-parser');
 const dbfuns_sql=require('../utils/dbfuns_sql');
+const dbfuns=require('../utils/dbfuns')
 const router = require('express').Router();
 
-router.get('/add',function (req,res) {
+router.use(bp.json());
+router.use(bp.urlencoded({extended: true}))
+
+
+
+router.use('/', express.static(__dirname.substr(0, __dirname.length - 7) + 'public_html/AddAssignment'));
+router.post('/add',function (req,res) {
     // dbfuns_sql.addcourse("Elixir","Arnav Gupta","abhishek1208@gmail.com;varun@gmail.com;aditi23@gmail.com",
     //     function (){}
     //
     // );
-    dbfuns_sql.addcourse("Pandora","Arnav Gupta","abhishek1208@gmail.com;varun@gmail.com;aditi23@gmail.com",
-        function (){}
+    // dbfuns_sql.addcourse("Pandora","Arnav Gupta","abhishek1208@gmail.com;varun@gmail.com;aditi23@gmail.com",
+    //     function (){}
+    //
+    // );
+    // dbfuns_sql.addasgn(req.body.name,req.body.desc,req.body.courseid);
+    // res.send("added course");
 
-    );
-    res.send("added course");
+    dbfuns.addasgn(req.body.name,req.body.desc,req.body.courseid);
 })
-router.get('/delete',function (req,res) {
-    dbfuns_sql.endcourse(6);
-    res.send("ended course");
-})
-router.get('/addstud',function (req,res) {
-    dbfuns_sql.addStudent(5,"ishan@gmail.com",function () {
-    });
-    res.send("Student Added");
-})
-router.get('/addAssgn',function (req,res) {
+// router.get('/delete',function (req,res) {
+//     dbfuns_sql.endcourse(6);
+//     res.send("ended course");
+// })
+// router.get('/addstud',function (req,res) {
+//     dbfuns_sql.addStudent(5,"ishan@gmail.com",function () {
+//     });
+//     res.send("Student Added");
+// })
+// router.get('/addAssgn',function (req,res) {
+//
+//     // dbfuns_sql.addasgn("assignmentabc","make a todolist",5);
+//     dbfuns_sql.addasgn("assignment3","SQL",6);
+//
+//     res.send("Assignment added");
+//
+// })
+//
+// router.get('/submitAssgn',function (req,res) {
+//
+//     dbfuns_sql.submitasgn(5,1,"ishan@gmail.com","www.github.com/assignment");
+//     res.send("Submission added");
+//
+// })
 
-    // dbfuns_sql.addasgn("assignmentabc","make a todolist",5);
-    dbfuns_sql.addasgn("assignment3","SQL",6);
-
-    res.send("Assignment added");
-
-})
-
-router.get('/submitAssgn',function (req,res) {
-
-    dbfuns_sql.submitasgn(5,1,"ishan@gmail.com","www.github.com/assignment");
-    res.send("Submission added");
-
-})
 module.exports = router;
